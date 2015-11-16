a2f708a431d1f032536100d99fb7bc8e4b4bd123
rémi  <remirobert33530@gmail.com>
2015-11-16 23:15:58 +0800

    update

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ function sendWelcomeUser(user) {
 
 app.get('/sms', function (req, res) {
   console.log("------------ [RECEIVED MESSAGE] -------");
-  console.log(req.IncomingMessage.query.Body);
+  console.log(req.query.Body);
   console.log(res);
   var phoneNumber = "+131";
   var command = "hello".toUpperCase().toLowerCase();  
