834c19a9ca87401dc58ef739397a200b6ab08159
rémi  <remirobert33530@gmail.com>
2015-11-17 11:06:24 +0800

    clean depot add ios app 📱

---
+++ FastlneIOS/FastlneIOS/CustomersController.swift
@@ -0,0 +1,14 @@
+//
+//  CustomersController.swift
+//  FastlneIOS
+//
+//  Created by Remi Robert on 17/11/15.
+//  Copyright © 2015 Remi Robert. All rights reserved.
+//
+
+import UIKit
+
+class CustomersController: NSObject {
+
+    
+}
